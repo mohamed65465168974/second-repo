008b3250a18177991239759b1af1aa33445ab94b
mohamed <mohamedshalaby0097@gmail.com>
2020-11-08 02:12:28 +0200

    Reducing the player speed when it is moving
    checking if the player is standing -> give it normal speed
    if the player is already moving now -> reduce it is speed

--- learnunityandgit/Assets/Scripts/movePlayer.cs
+++ learnunityandgit/Assets/Scripts/movePlayer.cs
@@ -8,6 +8,9 @@ public class movePlayer : MonoBehaviour {
 	private Rigidbody2D reg;
 	//public variable to adjust speed of the player 
 	public float speed = 10f;
+	//checking if the player is standing or not 
+	public bool standing;
+	public float speedSlow = 0.3f;	
 	// Use this for initialization
 	void Start () {
 		
@@ -17,15 +20,21 @@ public class movePlayer : MonoBehaviour {
 	void Update () {
 		//initializing the rigidbody component 
 		reg = GetComponent<Rigidbody2D>();
+		float forceY = Mathf.Abs (reg.velocity.y);
+		if (forceY < 0.2f)  //if the player is standing 
+			standing = true;
+		else
+			standing= false;
+		
 		//variable for the point we want to move to 
 		var newX = 0f;
 		var newY = 0f;
 		if (Input.GetKey ("right")) { //when clicking the right button in keyboard 
-			newX = speed;
+			newX = standing? speed:speed*speedSlow; //if the player standing move in normal speed and if it is already moving reduce it is speed 
 			transform.localScale = new Vector3 (1, 1, 1);
 		}
 		else if (Input.GetKey ("left")) { //when clicking the left button in keyboard 
-			newX = -speed;
+			newX = standing? -speed:-speed*speedSlow; //if the player standing move in normal speed and if it is already moving reduce it is speed 
 			transform.localScale = new Vector3 (-1, 1, 1);
 		}
 		else if (Input.GetKey ("up")) { //when clicking the up button in keyboard 
